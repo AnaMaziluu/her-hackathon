1a34914f622f60fca7bc52849c4ac3d7d425a03a
AnaMaziluu <anamazilu6@gmail.com>
2022-11-15 13:20:23 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import UserList from './UserList';
+import { users } from './users';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='header tc'>
+            <div className='flex dib m-2'>
+                <img className='w4 h2 ' alt='logo' src='https://www.accenture.com/t20180820T080700Z__w__/gb-en/_acnmedia/Accenture/Dev/Redesign/Acc_Logo_Black_Purple_RGB.PNG?'/>
+                <h1>knowledge for all</h1>
+            </div>
+            <Navbar />
+            <p className='d-flex justify-content-start pa4 fs-2 fw-bold'>Posts</p>
+            <UserList users={users}/>
+        </div>
+        
+    );
 }
 
 export default App;
